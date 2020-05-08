4ec7214c937d3eead0c3b5535b165c24b35729c0
fedemiguez <fedeemiguez@gmail.com>
2020-05-08 10:30:05 -0300

    se crean las pages y los demas componentes

--- src/index.js
+++ src/index.js
@@ -4,36 +4,12 @@
 // const container = document.getElementById('root')
 // container.appendChild(element)
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.css'
 
-const user = {
-  firstname : 'Fede',
-  lastname : 'Miguez',
-  avatar : 'https://img2.freepng.es/20180523/tha/kisspng-businessperson-computer-icons-avatar-clip-art-lattice-5b0508dc6a3a10.0013931115270566044351.jpg'
-}
+import App from './components/App'
 
-function getName(user){
-  return `${user.firstname} ${user.lastname}`
-}
-
-function getGreeting(user){
-  if(user){
-    return <h1> Hola {getName(user)} </h1>
-  }
-  return <h1> Hola Extrano </h1>
-}
-
-// const element = <div> {getGreeting(user)} </div>
-const element = (
-  <div>
-    <h1>{getGreeting(user)}</h1>
-    <img src={user.avatar}/>
-  </div>
-)
 const container = document.getElementById('root')
+ReactDOM.render( <App />,container)
 
-
-
-
-ReactDOM.render(element,container)
